aaeb2ee263d80172374e983145bee5bc1b7e5e17
Adidtiya Putra Caniago <51223543+Deviantkay@users.noreply.github.com>
2023-04-14 08:58:20 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,6 +112,87 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+/*==================== CONTACT FORM ====================*/
+const form = document.getElementById('my-form');
+const nameInput = document.getElementById('name');
+const cityInput = document.getElementById('city');
+const emailInput = document.getElementById('email');
+const zipCodeInput = document.getElementById('zip-code');
+const statusCheckbox = document.getElementById('status');
+const warningDiv = document.getElementById('warning');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  submit();
+});
+
+function handleGetFormData() {
+  const formData = {
+    name: nameInput.value,
+    city: cityInput.value,
+    email: emailInput.value,
+    zipCode: zipCodeInput.value,
+    status: statusCheckbox.checked
+  };
+
+  return formData;
+}
+
+function isNumber(str) {
+  return !isNaN(str);
+}
+
+function checkboxIsChecked() {
+  return statusCheckbox.checked;
+}
+
+function validateFormData(formData) {
+  if (!formData) {
+    return false;
+  }
+
+  if (formData.name === '') {
+    return false;
+  }
+
+  if (formData.city === '') {
+    return false;
+  }
+
+  if (formData.email === '') {
+    return false;
+  }
+
+  if (formData.zipCode === '') {
+    return false;
+  }
+
+  if (!isNumber(formData.zipCode)) {
+    return false;
+  }
+
+  if (!checkboxIsChecked()) {
+    return false;
+  }
+
+  return true;
+}
+
+function submit() {
+  const formData = handleGetFormData();
+  const isFormValid = validateFormData(formData);
+
+  if (isFormValid) {
+    warningDiv.textContent = '';
+    return;
+  }
+  else {
+    warningDiv.textContent = 'Periksa form anda sekali lagi';
+    return;
+  }
+}
+
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
     distance: '60px',
@@ -131,12 +212,12 @@ sr.reveal(`.home__data, .home__social-link, .home__info,
 })
 
 sr.reveal(`.about__data, 
-           .video__description,`,{
+           .video__description, .contact-title`,{
     origin: 'left',
 })
 
 sr.reveal(`.about__img-overlay, 
-           .video__content,`,{
+           .video__content, .name, .city, .email, .zipCode, .check, #submit-form`,{
     origin: 'right',
     interval: 100,
 })
